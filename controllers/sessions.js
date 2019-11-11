36ffceaf6dd2861be764a7aa5e7a22afef993da8
Wincy LAW <wincylaw@Wincys-MacBook-Pro.local>
2019-11-10 18:16:07 -0800

    added login and signup warnings

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -4,18 +4,20 @@ const User = require("../models/users.js")
 const bcrypt = require("bcrypt");
 
 router.get("/new", (req, res) => {
-  res.render("sessions/new.ejs");
+  res.render("sessions/new.ejs", {failure: req.session.failure});
 })
 
 router.post("/", (req, res) => {
   User.findOne({username: req.body.username}, (error, foundUser) => {
     if (foundUser===null) {
+      req.session.failure = "nullUser";
       res.redirect("/sessions/new");
     } else {
       if (bcrypt.compareSync(req.body.password, foundUser.password)) {
         req.session.username = foundUser.username;
         res.redirect("/logs")
       } else {
+        req.session.failure = "wrongPassword";
         res.redirect("/sessions/new");
       }
     }
--- controllers/users.js
+++ controllers/users.js
@@ -4,14 +4,15 @@ const bcrypt = require("bcrypt");
 const User = require("../models/users.js")
 
 router.get("/new", (req, res) => {
-  res.render("users/new.ejs")
+  res.render("users/new.ejs", {failure: req.session.failure})
 })
 
 router.post("/", (req, res) => {
   req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
   User.create(req.body, (error, createdUser) => {
     if (error) {
-      res.send("User name taken");
+      req.session.failure = "usernameTaken";
+      res.redirect("/users/new");
     } else {
       req.session.username = createdUser.username;
       res.redirect("/logs")
